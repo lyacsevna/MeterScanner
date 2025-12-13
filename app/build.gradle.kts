509c2a9aa7262d9abceae2f2cfbb2f64f4ed4daa
Sofia <sonechka68zvr@gmail.com>
2025-12-13 09:41:58 +0300

    docs: added course work docs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")  // ДОБАВЬТЕ ЭТУ СТРОКУ
+    id("org.jetbrains.kotlin.plugin.compose")
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
@@ -54,10 +54,8 @@ android {
 }
 
 dependencies {
-
-    // Базовые зависимости Compose
     implementation("androidx.navigation:navigation-compose:2.7.7")
-
+    implementation ("androidx.navigation:navigation-runtime-ktx:2.7.7")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("com.jakewharton.threetenabp:threetenabp:1.4.6")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
@@ -67,16 +65,11 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.foundation)
-
-    // Навигация и ViewModel
     implementation("androidx.navigation:navigation-compose:2.9.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
-
-    // Room Database
     implementation("androidx.room:room-runtime:2.8.4")
     implementation("androidx.room:room-ktx:2.8.4")
     implementation(libs.androidx.compose.foundation)
@@ -86,25 +79,16 @@ dependencies {
     implementation(libs.androidx.compose.ui.geometry)
     implementation(libs.protolite.well.known.types)
     implementation(libs.androidx.compose.ui.text)
+    implementation(libs.androidx.ui)
     kapt("androidx.room:room-compiler:2.8.4")
-
-    // Hilt
     implementation("com.google.dagger:hilt-android:2.57.2")
     kapt("com.google.dagger:hilt-compiler:2.57.2")
     implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
-
-    // CameraX
     implementation("androidx.camera:camera-camera2:1.5.1")
     implementation("androidx.camera:camera-lifecycle:1.5.1")
     implementation("androidx.camera:camera-view:1.5.1")
-
-    // ML Kit Text Recognition
     implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.1")
-
-    // Coil для изображений
     implementation("io.coil-kt:coil-compose:2.7.0")
-
-    // Тестовые зависимости
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
@@ -112,27 +96,17 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    // CameraX
     implementation("androidx.camera:camera-core:1.3.3")
     implementation("androidx.camera:camera-camera2:1.3.3")
     implementation("androidx.camera:camera-lifecycle:1.3.3")
     implementation("androidx.camera:camera-view:1.3.3")
     implementation("androidx.camera:camera-extensions:1.3.3")
-
-    // ML Kit для распознавания текста
     implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
     implementation("com.google.mlkit:text-recognition:16.0.0")
-
-    // Для работы с файлами
     implementation("androidx.exifinterface:exifinterface:1.3.7")
-
-    // Для удобного доступа к файловой системе
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.documentfile:documentfile:1.0.1")
-
     implementation("com.patrykandpatrick.vico:compose:1.16.0")
-    // Если вы используете Material 3, добавьте также:
     implementation("com.patrykandpatrick.vico:compose-m3:1.16.0")
 
 
